30144dc5ad614f2a24779637ddde1cba4a908267
Yuxin Lin <linyuxin0615@gmail.com>
2025-11-19 18:36:48 -0500

    test on ca-GrQc, proved that it works

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -8,7 +8,6 @@ def read_cora(content_path="./cora/cora.content", cites_path="./cora/cora.cites"
     node_ids = content[:, 0]
     id_map = {nid: i for i, nid in enumerate(node_ids)}
     n = len(node_ids)
-    print("num nodes =", n)
 
     edges = np.genfromtxt(cites_path, dtype=str)
     rows = []
@@ -76,3 +75,30 @@ def read_CAHepPh(ca_hepph_path="./CA-HepPh.txt"):
     print("After GC:", adj.shape, "nnz =", adj.nnz)
     
     return adj
+
+
+def read_CAGrQc(ca_grqc_path="./CA-GrQc.txt"):
+    edges = []
+    with open(ca_grqc_path) as f:
+        for line in f:
+            if line.startswith("#"):
+                continue
+            u, v = map(int, line.split())
+            edges.append((u, v))
+
+    edges = [(u, v) for (u, v) in edges if u != v]
+
+    nodes = sorted({u for e in edges for u in e})
+    id2idx = {nid: i for i, nid in enumerate(nodes)}
+    rows = [id2idx[u] for (u, v) in edges]
+    cols = [id2idx[v] for (u, v) in edges]
+
+    n_all = len(nodes)
+    adj_full = sp.sparse.coo_matrix((np.ones(len(rows)), (rows, cols)), shape=(n_all, n_all))
+
+    adj_full = adj_full + adj_full.T
+    adj_full.data[:] = 1.0
+    adj_full = adj_full.tocsr()
+    print("Shape:", adj_full.shape, "nnz =", adj_full.nnz)
+
+    return adj_full
