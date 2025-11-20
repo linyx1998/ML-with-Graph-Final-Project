e3dbf25331c26f91623b32985a0fe8a0375eb840
Yuxin Lin <linyuxin0615@gmail.com>
2025-11-19 19:28:24 -0500

    updated some datasets

--- DataPreprocess.py
+++ DataPreprocess.py
@@ -39,10 +39,13 @@ def read_cora(content_path="./cora/cora.content", cites_path="./cora/cora.cites"
     n = adj.shape[0]
     print("After GC:", adj.shape, "nnz =", adj.nnz)
     
+    mean_deg = adj.sum(axis=1).A1.mean()
+    print("Mean degree =", mean_deg)
+    
     return adj
 
 
-def read_CAHepPh(ca_hepph_path="./CA-HepPh.txt"):
+def read_cahepph(ca_hepph_path="./CA-HepPh.txt"):
     edges = []
     with open(ca_hepph_path) as f:
         for line in f:
@@ -74,10 +77,13 @@ def read_CAHepPh(ca_hepph_path="./CA-HepPh.txt"):
     n = adj.shape[0]
     print("After GC:", adj.shape, "nnz =", adj.nnz)
     
+    mean_deg = adj.sum(axis=1).A1.mean()
+    print("Mean degree =", mean_deg)
+    
     return adj
 
 
-def read_CAGrQc(ca_grqc_path="./CA-GrQc.txt"):
+def read_cag_fb_wiki(ca_grqc_path="./CA-GrQc.txt"):
     edges = []
     with open(ca_grqc_path) as f:
         for line in f:
@@ -94,11 +100,14 @@ def read_CAGrQc(ca_grqc_path="./CA-GrQc.txt"):
     cols = [id2idx[v] for (u, v) in edges]
 
     n_all = len(nodes)
-    adj_full = sp.sparse.coo_matrix((np.ones(len(rows)), (rows, cols)), shape=(n_all, n_all))
+    adj = sp.sparse.coo_matrix((np.ones(len(rows)), (rows, cols)), shape=(n_all, n_all))
 
-    adj_full = adj_full + adj_full.T
-    adj_full.data[:] = 1.0
-    adj_full = adj_full.tocsr()
-    print("Shape:", adj_full.shape, "nnz =", adj_full.nnz)
+    adj = adj + adj.T
+    adj.data[:] = 1.0
+    adj = adj.tocsr()
+    print("Shape:", adj.shape, "nnz =", adj.nnz)
+    
+    mean_deg = adj.sum(axis=1).A1.mean()
+    print("Mean degree =", mean_deg)
 
-    return adj_full
+    return adj
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 File List:
 
-* data: graphs that we can find
+* data: graphs that we can find, some are from https://snap.stanford.edu/data/index.html
 * DataPreprocess.py: preprocess the graphs to adj matrix
 * toy-graph.ipynb: reproduce figure 1
 * recover-triangles.ipynb: reproduce figure 4
